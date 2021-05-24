35f0fe69d62ff582795fbc772521d7d403b9dd7a
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-05-24 12:17:33 +0530

    Architecture changes

---
+++ README.md
@@ -0,0 +1,1 @@
+# EDITOR For Personal Project
---
+++ script.js
@@ -0,0 +1,36 @@
+let editor;
+ClassicEditor.create(document.querySelector("#editor"), {
+  toolbar: ["heading", "|", "bold", "italic", "bulletedList", "numberedList"],
+  heading: {
+    options: [
+      {
+        model: "paragraph",
+        title: "Paragraph",
+        class: "ck-heading_paragraph",
+      },
+      {
+        model: "heading1",
+        view: "h1",
+        title: "Heading 1",
+        class: "ck-heading_heading1",
+      },
+      {
+        model: "heading2",
+        view: "h2",
+        title: "Heading 2",
+        class: "ck-heading_heading2",
+      },
+    ],
+  },
+})
+  .then((nEditor) => {
+    editor = nEditor;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+document.querySelector("#submit").addEventListener("click", () => {
+  document.querySelector("#content").innerHTML = editor.getData();
+  console.log(editor.getData());
+});
